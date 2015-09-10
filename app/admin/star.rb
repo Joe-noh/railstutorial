a12d529f5297e1ca53d495d735e3cf428d739ade
Joe-noh <goflb.jh@gmail.com>
2015-09-10 12:50:19 +0900

    管理画面のstarフォームのstatusをinput selectに変更

--- app/admin/star.rb
+++ app/admin/star.rb
@@ -1,4 +1,14 @@
 ActiveAdmin.register Star do
+  permit_params :user_id, :date, :status
+
+  form do |f|
+    f.inputs "Star" do
+      input :user
+      input :date
+      input :status, as: :select, collection: [:candidate, :accepted, :declined]
+    end
+    f.actions
+  end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
