1b31f8b2c040ab8af41e2697fa3736a078b26cc6
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-09-14 11:36:30 +0900

    PATCH /api/stardom を追加

--- app/controllers/api/stardom_controller.rb
+++ app/controllers/api/stardom_controller.rb
@@ -12,4 +12,20 @@ class Api::StardomController < Api::ApplicationController
 
     render json: {active: active, star_status: status}
   end
+
+  def update
+    today = shifted_date
+
+    accept = params[:stardom] && params[:stardom][:accept]
+    if accept != 'true' && accept != 'false'
+      render_errors ['Unacceptable value for stardom[accept]'], status: :bad_request
+    else
+      if star = current_user.stars.find_by(date: today)
+        star.update(status: (accept != 'false' ? :accepted : :declined))
+        render json: {}, status: :ok
+      else
+        render_errors ['You are not elected for a star.'], status: :forbidden
+      end
+    end
+  end
 end
